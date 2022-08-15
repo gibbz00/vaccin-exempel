61593985acc6a53e26b5c9d046175f9a59666367
Gabriel Hansson <gabrielhansson00@gmail.com>
2022-08-15 15:42:20 +0000

    styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { get } from 'svelte/store'
-	import Layout from './__layout.svelte'
+	// Tailwind styles
+	import '../app.css'
 
 	// Lista med vaccin som finns
 	let existingVaccines: string[] = ['COVID-19', 'Hepatit A', 'Mässling', 'Polio']
@@ -45,46 +45,70 @@
 	console.log(takenVaccines.includes('COVID-19'))
 </script>
 
-<h1 class="text-6xl">Vaccinkoll</h1>
+<body class="bg-blue-800 w-screen h-screen grid place-content-center">
+	<main class="m-10 p-8 bg-zinc-100 w-max">
+		<h1 class="text-5xl mb-5">Vaccinkoll</h1>
 
-<hr />
-<!-- Visa alla vaccin -->
-{#each existingVaccines as vaccine (vaccine)}
-	<div>
-		{vaccine}
-		<input type="checkbox" bind:group={takenVaccines} value={vaccine} />
-		<span on:click={() => removeVaccine(vaccine)}>X</span>
-	</div>
-{/each}
+		<div class="flex md:space-x-4 flex-col md:flex-row">
+			<section class="bg-white p-5 shadow-md">
+				<!-- Visa alla vaccin -->
+				<h2 class="text-2xl mb-1">Vaccinlista</h2>
+				{#each existingVaccines as vaccine (vaccine)}
+					<div class="text-lg flex">
+						<input type="checkbox" bind:group={takenVaccines} value={vaccine} />
+						<div class="ml-2 group flex">
+							{vaccine}
+							<button
+								class="hidden group-hover:block ml-2 text-gray-500"
+								on:click={() => removeVaccine(vaccine)}>X</button
+							>
+						</div>
+					</div>
+				{/each}
 
-<!-- Lägg till vaccin -->
-<input class="border-2" placeholder="Nytt vaccin" type="text" bind:value={newVaccine} />
-<button on:click={addVaccine}>Lägg till</button>
+				<!-- Lägg till vaccin -->
+				<div class="mt-3 mb-1">Lägg till ett nytt vaccin</div>
+				<div class="flex">
+					<input
+						class="border-2 mr-2"
+						placeholder="Namn"
+						type="text"
+						bind:value={newVaccine}
+					/>
+					<button class="border-2 py-1 px-2" on:click={addVaccine}>Skicka</button>
+				</div>
+			</section>
 
-<hr />
+			<section class="bg-white p-5 mt-5 md:mt-0 shadow-md flex flex-col">
+				<div class="flex contents-center mb-2">
+					<div class="self-center">
+						<h2 class="text-2xl pr-3 text-center">Välj land:</h2>
+					</div>
+					<select
+						class="px-2 py-2 bg-blue-800 text-white font-semibold"
+						bind:value={chosenCountry}
+					>
+						{#each [...countryVaccineInfo.keys()] as country}
+							<option value={country}>{country} </option>
+						{/each}
+					</select>
+				</div>
 
-<!-- Visa att det går att välja vaccin -->
-<h2 class="text-md">Tagna vaccin</h2>
-{#each takenVaccines as vaccine}
-	<div>{vaccine}</div>
-{/each}
-
-<hr />
-
-<h2>Välj land:</h2>
-<select bind:value={chosenCountry}>
-	{#each [...countryVaccineInfo.keys()] as country}
-		<option value={country}>{country} </option>
-	{/each}
-</select>
-
-{#if chosenCountry != undefined}
-	{#each [...countryVaccineInfo.get(chosenCountry)] as recommendedVaccines}
-		<dir>
-			{recommendedVaccines}
-		</dir>
-		{#if !takenVaccines.includes(recommendedVaccines)}
-			Inte taget!
-		{/if}
-	{/each}
-{/if}
+				{#if chosenCountry != undefined}
+					<ul class="border-2 h-full p-4">
+						{#each [...countryVaccineInfo.get(chosenCountry)] as recommendedVaccines}
+							<li class="text-lg">
+								{recommendedVaccines}
+								{#if takenVaccines.includes(recommendedVaccines)}
+									<span class="text-green-600"> ✓ </span>
+								{:else}
+									<span class="text-red-700"> ✗ </span>
+								{/if}
+							</li>
+						{/each}
+					</ul>
+				{/if}
+			</section>
+		</div>
+	</main>
+</body>
